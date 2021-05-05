a38ba92871c8960648be7b179c8ab9e97666edea
ZekeMarshall <marshall_zeke@outlook.com>
2021-05-05 09:42:14 +0100

    Changing cache path, updating renv lockfile

--- App-Modules/load_drake_data.R
+++ App-Modules/load_drake_data.R
@@ -7,41 +7,44 @@ library(matsbyname)
 library(matsindf)
 library(tidyverse)
 library(IEATools)
+library(PFUSetup)
 
 ################################################################################
 ## Downloading data from drake cache
 ################################################################################
 
 ## Establishes path to the drake cache
-# Set up the root directory
-root <- rprojroot::is_rstudio_project
+cache_path <- PFUSetup::get_abs_paths()$drake_cache_folder
 
-# Identifies the file path for the drake cache
-cache_path <- root$find_file(".drake")
+# # Set up the root directory
+# root <- rprojroot::is_rstudio_project
+#
+# # Identifies the file path for the drake cache
+# cache_path <- root$find_file(".drake")
 
 # Creates a list of the countries in the drake workflow, set in the _drake.R script
-countries <- drake::readd(SEAPSUTWorkflow::target_names$countries, 
-                          path = cache_path, 
+countries <- drake::readd(SEAPSUTWorkflow::target_names$countries,
+                          path = cache_path,
                           character_only = TRUE)
 
 # Creates the data frame for the geographical map coverage.
 map_data <- data.frame(name = countries, color = countries)
 
 # Creates a list of years in the drake workflow
-max_year <- drake::readd(SEAPSUTWorkflow::target_names$max_year, 
-                         path = cache_path, 
+max_year <- drake::readd(SEAPSUTWorkflow::target_names$max_year,
+                         path = cache_path,
                          character_only = TRUE)
 
 # Creates a list of years
 years <- paste(1960:max_year)
 
-# Creates a df with the Balanced IEA Data for the selected countries  
-balanced_iea_data <- drake::readd(SEAPSUTWorkflow::target_names$BalancedIEAData, 
-                                  path = cache_path, 
+# Creates a df with the Balanced IEA Data for the selected countries
+balanced_iea_data <- drake::readd(SEAPSUTWorkflow::target_names$BalancedIEAData,
+                                  path = cache_path,
                                   character_only = TRUE)
 # Loads Machine data target
-MachineData <- drake::readd(SEAPSUTWorkflow::target_names$AllMachineData, 
-                            path = cache_path, 
+MachineData <- drake::readd(SEAPSUTWorkflow::target_names$AllMachineData,
+                            path = cache_path,
                             character_only = TRUE)
 
 
@@ -51,30 +54,30 @@ etaphi_data <- drake::readd(SEAPSUTWorkflow::target_names$CompletedEfficiencyTab
                             character_only = TRUE)
 
 # Creates a df with the Destination-Machine&Eu.product allocation data target
-allocations_data <- drake::readd(SEAPSUTWorkflow::target_names$CompletedAllocationTables, 
-                                 path = cache_path, 
+allocations_data <- drake::readd(SEAPSUTWorkflow::target_names$CompletedAllocationTables,
+                                 path = cache_path,
                                  character_only = TRUE)
 
 # Adds a combined Machine-Eu.product column
 allocations_data$Machine_Eu.product = paste(allocations_data$Machine," - ", allocations_data$Eu.product)
 
 # Reads PSUT_useful target, also contains PSUT_final data target
-PSUT_useful_data <- drake::readd(SEAPSUTWorkflow::target_names$PSUT_useful, 
-                                 path = cache_path, 
+PSUT_useful_data <- drake::readd(SEAPSUTWorkflow::target_names$PSUT_useful,
+                                 path = cache_path,
                                  character_only = TRUE)
 
 # Reads aggregate final and useful energy/exergy data target
-Agg_finaluseful_data <- drake::readd(SEAPSUTWorkflow::target_names$AggregateFinalUsefulData, 
-                                     path = cache_path, 
+Agg_finaluseful_data <- drake::readd(SEAPSUTWorkflow::target_names$AggregateFinalUsefulData,
+                                     path = cache_path,
                                      character_only = TRUE)
 
 # Reads aggregate primary energy/exergy data target
-Agg_primary_data <- drake::readd(SEAPSUTWorkflow::target_names$AggregatePrimaryData, 
-                                     path = cache_path, 
+Agg_primary_data <- drake::readd(SEAPSUTWorkflow::target_names$AggregatePrimaryData,
+                                     path = cache_path,
                                      character_only = TRUE)
 # Reads socioeconomic data target
-Socioecon_data <- drake::readd(SEAPSUTWorkflow::target_names$SocioEconData, 
-                               path = cache_path, 
+Socioecon_data <- drake::readd(SEAPSUTWorkflow::target_names$SocioEconData,
+                               path = cache_path,
                                character_only = TRUE)
 
 
@@ -103,7 +106,7 @@ PSUT_metrics_total_iyear <- PSUT_metrics_total %>%
   dplyr::group_by(Country, Method, Energy.type, Stage, Gross.Net) %>%
   dplyr::filter(Year == min(Year)) %>%
   dplyr::select(-E.product, -Flow.Sector, -Aggregation.by, -Year) %>%
-  magrittr::set_colnames(c("Country", "Method", "Energy.type", "Stage", 
+  magrittr::set_colnames(c("Country", "Method", "Energy.type", "Stage",
                            "Gross.Net", "EX.iyear")) %>%
   dplyr::ungroup()
 
@@ -122,14 +125,14 @@ PSUT_etas <- PSUT_metrics_total_i %>%
   dplyr::select(-EX.i, -Aggregation.by) %>%
   tidyr::pivot_wider(names_from = Stage,
                      values_from = EX) %>%
-  dplyr::group_by(Country, Method, Energy.type, Year) %>% 
+  dplyr::group_by(Country, Method, Energy.type, Year) %>%
   tidyr::fill(Primary, .direction = "down", .by_group = TRUE) %>%
   dplyr::filter(Gross.Net != "Neither") %>%
   dplyr::group_by(Country, Method, Energy.type, Gross.Net, Year) %>%
   dplyr::mutate("Eta.fu" = Useful / Final) %>%
   dplyr::mutate("Eta.pf" = Final / Primary) %>%
   dplyr::mutate("Eta.pu" = Useful / Primary) %>%
-  dplyr::select(Country, Method, Energy.type, 
+  dplyr::select(Country, Method, Energy.type,
                 Gross.Net, Year, Eta.fu, Eta.pf, Eta.pu) %>%
   tidyr::pivot_longer(cols = Eta.fu:Eta.pu,
                       names_to = "Stages",
@@ -143,7 +146,7 @@ PSUT_etas <- PSUT_metrics_total_i %>%
 
 # Calculates index values for each metric in PSUT_etas
 PSUT_etas_indexes <- PSUT_etas %>%
-  dplyr::group_by(Country, Method, Energy.type, Gross.Net, Stages) %>% 
+  dplyr::group_by(Country, Method, Energy.type, Gross.Net, Stages) %>%
   dplyr::filter(Year == min(Year)) %>%
   dplyr::select(-Year) %>%
   magrittr::set_colnames(c("Country", "Method", "Energy.type",
@@ -152,7 +155,7 @@ PSUT_etas_indexes <- PSUT_etas %>%
 
 # Adds index values to PSUT_etas tibble, and calculated yearly indexed data
 PSUT_etas <- PSUT_etas %>%
-  dplyr::left_join(PSUT_etas_indexes, by = c("Country", "Method", "Energy.type", "Gross.Net", "Stages")) %>% 
+  dplyr::left_join(PSUT_etas_indexes, by = c("Country", "Method", "Energy.type", "Gross.Net", "Stages")) %>%
   dplyr::mutate("Eta.i" = Eta / Eta.index) %>%
   dplyr::select(-Eta.index)
 
@@ -173,18 +176,18 @@ PSUT_etas <- PSUT_etas %>%
 
 # PSUT_useful_USA <- PSUT_useful_data %>%
 #   dplyr::filter(Country == "USA")
-# 
+#
 # PSUT_useful_USA_2010 <- PSUT_useful_data %>%
 #   dplyr::filter(Country == "USA", Year == 2010)
-# 
+#
 # PSUT_nea_data <- PSUT_useful_USA_2010 %>%
 #   Recca::calc_io_mats() %>%
 #   Recca::calc_ERRs_gamma() %>%
 #   Recca::calc_E_EIOU() %>%
 #   Recca::calc_erois() %>%
-#   dplyr::select(Country, Method, Energy.type, Last.stage, Year, 
-#                 ger_gamma, ner_gamma, r_gamma, E_EIOU, e_EIOU, 
-#                 eroi_g_p, eroi_n_p, eroi_g_i, eroi_n_i, eroi_g_p_feed, 
+#   dplyr::select(Country, Method, Energy.type, Last.stage, Year,
+#                 ger_gamma, ner_gamma, r_gamma, E_EIOU, e_EIOU,
+#                 eroi_g_p, eroi_n_p, eroi_g_i, eroi_n_i, eroi_g_p_feed,
 #                 eroi_n_p_feed, eroi_g_i_feed, eroi_n_i_feed)
 
 
--- app.R
+++ app.R
@@ -29,8 +29,10 @@ library(scales)
 library(gt)
 library(xtable)
 
-# Load Energy-Economy Decoupling related packages
 library(MKHthemes)
+library(colorspace)
+
+# Load Energy-Economy Decoupling related packages
 library(Recca)
 library(SEAPSUTWorkflow)
 library(PFUSetup)
@@ -85,21 +87,21 @@ source("App-Modules/guides_ee.R", local = TRUE)
 ################################################################################
 
 ui = dashboardPage(
-  
+
 ################################################################################
 # Header UI
 ################################################################################
-  
+
   # Custom css for app appearance
   # tags$head(
   #   tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")),
   # tags$head(includeCSS("www/custom.css")),
-  
+
   header = dashboardHeader(
-    
+
     # enable_rightsidebar = TRUE,
     # rightSidebbarIcon = "gears",
-    
+
     title = tags$h4(HTML("Energy-Economy <br/> Decoupling"),
                     style =
                     "
@@ -114,107 +116,107 @@ ui = dashboardPage(
                     color: #FFFFFF;
                     "
                     ),
-                  
+
                   # title = span("Energy-Economy Decoupling",
                   #              style = "color: white; font-size: 18px"),
                   # titleWidth = 300,
-                  
+
                   tags$li(a(href = 'http://www.leeds.ac.uk',
                             img(src = 'Leeds Logo White Text 1.png',
                                 title = "Leeds", height = "40px"),
-                            style = "padding-top:5px; 
-                            padding-bottom:5px; 
-                            padding-right:0px; 
-                            padding-left:0px; 
+                            style = "padding-top:5px;
+                            padding-bottom:5px;
+                            padding-right:0px;
+                            padding-left:0px;
                             margin-right:20px;"),
                           class = "dropdown"),
                   tags$li(a(href = 'https://calvin.edu/',
                             img(src = 'Calvin Logo White Text 1.png',
                                 title = "Calvin", height = "40px"),
-                            style = "padding-top:5px; 
-                            padding-bottom:5px; 
-                            padding-right:0px; 
-                            padding-left:0px; 
+                            style = "padding-top:5px;
+                            padding-bottom:5px;
+                            padding-right:0px;
+                            padding-left:0px;
                             margin-right:10px;"),
                           class = "dropdown")
                   ),
-  
+
 ################################################################################
 # Left Sidebar UI
 ################################################################################
-  
+
   sidebar = dashboardSidebar(
-    
+
     width = 272.25, # Header is 230px button is 42.5px
-    
+
     minified = FALSE,
-    
+
     sidebarMenu(id = "sidebarmenu",
-                
+
                 menuItem("Outline", tabName = "outline", icon = icon("chalkboard-teacher")),
-                
+
                 menuItem("PFU Database", expandedName = "pfu_menu", icon = icon("battery-quarter"),
-                         
+
                          menuItem("Introduction", tabName = "intro_pfu", icon = icon("book-reader")),
-                         
+
                          menuItem("PFU Dashboard", tabName = "dashboard_pfu", icon = icon("dashboard")),
-                         
+
                          menuItem("Energy Conversion Chain", tabName = "sankey", icon = icon("project-diagram")),
-                         
+
                          menuItem("Final-to-useful Allocations", tabName = "allocations", icon = icon("chart-pie")),
-                                  
+
                          menuItem("Final-to-useful Efficiencies", tabName = "eta_phi", icon = icon("chart-line")),
-                         
+
                          menuItem("Decomposition Analysis", tabName = "lmdi", icon = icon("search")),
-                         
+
                          menuItem("Database documentation", tabName = "documentation", icon = icon("book")),
-                         
+
                          menuItem("Citation", tabName = "citation_pfu", icon = icon("user-graduate"))
-                         
+
                          ),
                 menuItem("Energy-Economy", expandedName = "ee_menu", icon = icon("industry"),
-                
+
                          menuItem("Energy-Economy Dashboard", tabName = "dashboard_ee", icon = icon("dashboard")),
-                         
+
                          menuItem("Indexed Data", tabName = "index_data", icon = icon("chart-line")),
-                         
+
                          menuItem("Energy-GDP Coupling Space", tabName = "energy_gdp_space", icon = icon("link")),
-                         
+
                          menuItem("Energy-Efficiency Rebound Space", tabName = "rebound_space", icon = icon("compress-alt")),
-                         
+
                          menuItem("Causality Testing", tabName = "cause_test", icon = icon("file-invoice-dollar")),
-                         
+
                          menuItem("APF Fitting", tabName = "apf_fit", icon = icon("hand-holding-usd")),
-                         
+
                          menuItem("Citation", tabName = "citation_ee", icon = icon("user-graduate"))
-                         
+
                          ),
-                
+
                 menuItem("Rebound", expandedName = "rebound", icon = icon("compress-alt"),
-                         
+
                          menuItem("Introduction", tabName = "intro_rebound", icon = icon("book-reader")),
-                         
+
                          menuItem("Rebound Dashboard", tabName = "dashboard_rebound", icon = icon("dashboard")),
-                         
+
                          menuItem("Citation", tabName = "citation_rebound", icon = icon("user-graduate"))
-                         
+
                          ),
-                
+
                 menuItem("Net Energy Analysis", tabName = "netenergy", icon = icon("balance-scale")),
-                
+
                 menuItem("Related Resources", tabName = "relatedresources", icon = icon("book")),
-                
+
                 menuItem("Contact", tabName = "contact", icon = icon("phone-alt"))
-                
+
                 )
     ),
-  
+
   # sidebar_fullCollapse = TRUE,
 
 ################################################################################
 # Right Sidebar UI
 ################################################################################
-  
+
   # rightsidebar = rightSidebar(
   #   background = "dark",
   #   rightSidebarTabContent(
@@ -240,15 +242,15 @@ ui = dashboardPage(
   #     numericInput("obs", "Observations:", 10, min = 1, max = 100)
   #   )
   # ),
-  
+
   # title = "Right Sidebar",
-  
+
 ################################################################################
 # Body UI
 ################################################################################
-  
+
   body = dashboardBody(
-    
+
     # Reduce size of UI to 80% by default
     # Loading with whitespace below until new tab is opened
     # tags$style("body {
@@ -256,100 +258,100 @@ ui = dashboardPage(
     #            zoom: 0.9; /* Other non-webkit browsers */
     #            zoom: 90%; /* Webkit browsers */
     #            }"),
-    
+
     # Load custom theme from customTheme.R script
     customTheme,
-    
+
     tabItems(
-      
+
       # General
       tabItem(tabName = "outline",
               outlineUI(id = "outline1")),
-      
-      
+
+
       # PFU-Database items
       tabItem(tabName = "intro_pfu",
               intro_pfuUI(id = "intro2")),
-      
+
       tabItem(tabName = "dashboard_pfu",
-              sumdashplotsUI(id = "dash1")), 
-      
+              sumdashplotsUI(id = "dash1")),
+
       tabItem(tabName = "sankey",
               eccUI(id = "ecc1")),
-      
+
       tabItem(tabName = "allocations",
               allocplotsUI(id = "allocations1")),
-      
+
       tabItem(tabName = "eta_phi",
               etaphiplotsUI(id = "etaphi1")),
-      
+
       tabItem(tabName = "documentation",
               documentationUI(id = "doc1")),
-      
+
       # Energy-Economy items
       tabItem(tabName = "index_data",
-              fd_gdp_plotUI(id = "fd_gdp1")),  
-      
+              fd_gdp_plotUI(id = "fd_gdp1")),
+
       tabItem(tabName = "energy_gdp_space",
               decoupling_spaceUI(id = "ds1")),
-      
+
       tabItem(tabName = "rebound_space",
               rebound_spaceUI(id = "rs1")),
-      
+
       tabItem(tabName = "lmdi",
               decoupling_spaceUI(id = "lmdi1")),
-      
+
       tabItem(tabName = "dashboard_ee",
               sumdashplots_eeUI(id = "dash2")),
-      
-      
-      
+
+
+
       ## Rebound modules - UI
       tabItem(tabName = "intro_rebound",
               intro_reboundUI(id = "intro3")),
-      
+
       tabItem(tabName = "dashboard_rebound",
               rebound_dashUI(id = "dash3")),
-      
-      
-      
-      
+
+
+
+
       ## Other modules - UI
-      
+
       # Related resources UI
       tabItem(tabName = "relatedresources",
               relresourcesUI(id = "rr1"))
-      
+
       # Contact UI
-      
+
       ),
   )
 )
 
-  
+
 ################################################################################
 # Server
-################################################################################  
-  
+################################################################################
+
 server <- function(input, output, session) {
-  
+
 ################################################################################
 # Call guides
 ################################################################################
-  
+
   # # Initialise guide, but do not start automatically
   # guide_main$
   #   init()
-  # 
+  #
   # # Observes buttons which launch tours of app
   # observeEvent(input$guide_main_ID, {
   #   guide_main$start()
   # })
-  
+
   # Initialise guide_pfu, but do not start automatically
   guide_pfu$
     init()
-  
+
   # Triggers PFU Database guide when childless menuItem is opened
   observeEvent(input$sidebarItemExpanded, {
     req(input$sidebarItemExpanded)
@@ -357,15 +359,15 @@ server <- function(input, output, session) {
     guide_pfu$start()
     }
   })
-  
+
   # Initialise guide_pfu_dash, but do not start automatically
   guide_pfu_dash$
     init()
-  
+
   # Initialise guide_pfu, but do not start automatically
   guide_ee_dash$
     init()
-  
+
   # Triggers Energy-Economy dashboard guide when menuItem dashboard_ee is selected
   observeEvent(input$sidebarmenu, {
     req(input$sidebarmenu)
@@ -373,58 +375,58 @@ server <- function(input, output, session) {
       guide_ee_dash$start()
     }
   })
-  
+
 ################################################################################
 # Call server-side module components
 ################################################################################
-  
+
   ## PFU-Database modules
-  
+
   # Calls allocations.R module
-  callModule(module = allocplots, 
-             id = "allocations1", 
+  callModule(module = allocplots,
+             id = "allocations1",
              allocations_data)
-  
+
   # Calls etaphi.R module
   callModule(module = etaphiplots,
              id = "etaphi1",
              etaphi_data)
-  
+
   # Calls ecc.R module
   callModule(module = ecc,
              id = "ecc1",
              PSUT_useful_data)
-  
+
   # Calls sum_dash_pfu.R module
   callModule(module = sumdashplots,
              id = "dash1",
              PSUT_etas,
              PSUT_metrics_total,
              GDP_metrics)
-  
+
   # Calls decomposition module
-  
-  
+
+
   ## Energy-Economy modules
-  
+
   # Calls fd_gdp.R module
   callModule(module = fd_gdp_plot,
              id = "fd_gdp1",
              PSUT_metrics_total,
              GDP_metrics)
-  
+
   # Calls decoupling_space.R module
   callModule(module = decoupling_space,
              id = "ds1",
              PSUT_metrics_total,
              GDP_metrics)
-  
+
   # Calls rebound_space.R module
   callModule(module = rebound_space,
              id = "rs1",
              PSUT_metrics_total,
              PSUT_etas)
-  
+
   # Calls energy-economy dashboard module
   callModule(module = sumdashplots_ee,
              id = "dash2",
@@ -433,22 +435,22 @@ server <- function(input, output, session) {
              EX_Econ_Data,
              EA_data_final, #?
              APF_Data)
-  
-  # Calls causality module 
-  
-  
+
+  # Calls causality module
+
+
   ## Rebound modules
-  
+
   # Calls rebound introduction module
-  
+
   # Calls rebound dashboard module
-  
+
   callModule(module = rebound_dash,
              id = "dash3")
-  
-  # Calls rebound citation module 
-  
-  
+
+  # Calls rebound citation module
+
+
 }
 
 shinyApp(ui, server)
