236e9787ea324200c9c1b45748e8a44f97e7f5ea
ZekeMarshall <marshall_zeke@outlook.com>
2021-05-26 16:01:06 +0100

    Tidying ecc

--- App-Modules/ecc.R
+++ App-Modules/ecc.R
@@ -8,63 +8,80 @@ eccUI <- function(id) {
   ns <- NS(id)
   fluidRow(
 
-    box(title = "Energy Conversion Chain",
-        width = 12,
-        splitLayout(
-          
-          sliderInput(
-            inputId = ns("Year"),
-            label = "Year",
-            min = 1960,
-            max = max_year,
-            value = max_year,
-            step = 1,
-            sep = "",
-            width = "100%",
-            animate = TRUE),
-          
-          selectizeInput(
-            inputId = ns("Country"),
-            label = "Country:",
-            choices = countries,
-            width = "100%",
-            options = list(dropdownParent = 'body')),
-          
-          # style = "z-index:1002;",
-          
-          cellWidths = c("90%", "10%"),
-          
-          cellArgs = list(style = "vertical-align: center; 
+    box(
+      title = "Variables",
+      id = "Variables_pfu",
+      # status = "primary",
+      solidHeader = FALSE,
+      closable = FALSE,
+      collapsible = TRUE,
+      width = 12,
+      # height = 100,
+      splitLayout(
+
+        sliderInput(
+          inputId = ns("Year"),
+          label = "Year",
+          min = 1960,
+          max = max_year,
+          value = max_year,
+          step = 1,
+          sep = "",
+          width = "100%",
+          animate = TRUE),
+
+        selectizeInput(
+          inputId = ns("Country"),
+          label = "Country:",
+          choices = countries,
+          width = "100%",
+          options = list(dropdownParent = 'body')),
+
+        # style = "z-index:1002;",
+
+        cellWidths = c("90%", "10%"),
+
+        cellArgs = list(style = "vertical-align: center;
                                   padding-right: 6px;
                                   padding-left: 6px;
                                   padding-bottom: 0px;
                                   padding-top: 0px;"
-                          )), # horizontal-align: center
+        )
 
-        sankeyNetworkOutput(outputId = ns("sankey"),
-                            height = 900))
-      )
+      )),
+
+    box(
+      title = "Energy Conversion Chain",
+      id = "ccc",
+      width = 12,
+      sankeyNetworkOutput(outputId = ns("sankey"),
+                          height = 860)),
+
+    tags$head(tags$style('#ccc .box-header{ display: none}'))
+
+  ) # Closes fluidRow
+
 }
 
 # Establishes server module function
 ecc <- function(input, output, session,
-                Year, 
-                Country, 
+                Year,
+                Country,
                 sankey) {
 
-  
+
   # Creates reactive data frame for energy conversion chain from PSUT_useful target
   selected_data <- reactive({
-    dplyr::filter(PSUT_useful_data, 
-                  Country == input$Country, 
+    dplyr::filter(PSUT_useful_data,
+                  Country == input$Country,
                   Year == input$Year)
-    
+
   })
-  
-  
+
+
   # Energy conversion chain sankey diagrams
-  output$sankey <- renderSankeyNetwork({ 
-    
+  output$sankey <- renderSankeyNetwork({
+
     ecc_sankey <- selected_data() %>%
       Recca::make_sankey(nodeWidth = 20,
                          units = "ktoe",
@@ -73,24 +90,24 @@ ecc <- function(input, output, session,
                          ) %>%
       magrittr::extract2("Sankey") %>%
       magrittr::extract2(2)
-    
+
     # Add onRender JavaScript code
-    ecc_sankey <- htmlwidgets::onRender(ecc_sankey, 
+    ecc_sankey <- htmlwidgets::onRender(ecc_sankey,
       'function(el, x) {
-      
+
       // Select all node text
       d3.selectAll(".node text")
         .style("fill", "black") // Set text colour to black
         .style("font-size", "12px") // Change font size to 12
         .attr("text-anchor", "begin") // Set text node side to right
         .attr("x", 20);
-      
+
       }'
       )
-    
+
     ecc_sankey
-    
-    
+
+
   })
 
 }
